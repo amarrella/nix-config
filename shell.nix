e21c38a9646a4dc4e15a37acfcb057acbc396591
Alessandro Marrella <amarrella@earnestresearch.com>
2020-05-11 09:34:16 +0100

    use niv for niv

--- shell.nix
+++ shell.nix
@@ -1,13 +1,14 @@
 let
   sources = import ./nix/sources.nix;
   pkgs = import sources.nixpkgs {};
+  niv = import sources.niv { inherit pkgs; };
 
 in
   with pkgs;
   mkShell {
     inherit ((import ./default.nix).pre-commit-check) shellHook;
     buildInputs = [
-      pkgs.niv
+      niv.niv
       nixpkgs-fmt
     ];
   }
